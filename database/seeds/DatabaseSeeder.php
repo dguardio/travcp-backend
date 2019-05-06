abfa52618a1670a9e0828722698be926afb8ba84
TheDarkKid <laryho18@gmail.com>
2019-05-06 22:00:38 +0100

    edited db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             // BookableTypesTableSeeder::class,
-            ExperiencesTableSeeder::class,
+//            ExperiencesTableSeeder::class,
             // EventsTableSeeder::class,
             // RestaurantsTableSeeder::class,
             // FoodMenuTableSeeder::class
