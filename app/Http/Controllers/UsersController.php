8e42e96827ac2940ef13ffb78614c272755ffbd4
TheDarkKid <laryho18@gmail.com>
2019-05-20 16:07:00 +0100

    fixed image upload

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -95,7 +95,7 @@ class UsersController extends Controller
 
         // add upload
         $validated['upload_id'] = $upload_id;
-
+        unset($validated["profile_image"]);
         // add other user object properties
         $user->update($validated);
 
