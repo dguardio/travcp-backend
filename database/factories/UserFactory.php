c89d3e956bc938206a116d311e5431c1a5433e56
TheDarkKid <laryho18@gmail.com>
2019-05-31 12:09:27 +0100

    minor changes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use TCG\Voyager\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +15,13 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
+    $role = Role::inRandomOrder()->get()->first();
     return [
         'first_name' => $faker->firstName,
         'surname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'role' => 'user',
+        'role_id' => $role->id,
         'remember_token' => str_random(10),
     ];
 });
