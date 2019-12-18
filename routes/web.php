42340e2d7b4c8a6f62043b828eb19f01a83d6afe
Jude <judebiose20@gmail.om>
2019-12-19 00:08:57 +0100

    newsletter

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
-    Route::resource('admin/mail', 'MailController');
+    Route::resource('mail', 'MailController');
 });
 
 Route::get('/dashboard/messages', function() {
