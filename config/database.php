b785b2135dceee3ce7d15df1aa980a8cdb6233c3
Opeyemi <akinwoleolaopeyemi@gmail.com>
2020-03-21 03:53:39 +0100

    Removed local vendor folder and reinstall compser packages

--- config/database.php
+++ config/database.php
@@ -35,18 +35,18 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => 'DB_DATABASE', database_path('database.sqlite'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'DB_HOST', '127.0.0.1',
-            'port' => 'DB_PORT', '3306',
-            'database' => 'DB_DATABASE', 'forge',
-            'username' => 'DB_USERNAME', 'forge',
-            'password' => 'DB_PASSWORD', '',
-            'unix_socket' => 'DB_SOCKET', '',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
