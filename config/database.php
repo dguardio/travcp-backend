aa3fe271abde49f28d0eac18e6ab429f1c18d83c
Opeyemi <akinwoleolaopeyemi@gmail.com>
2020-03-21 05:04:29 +0100

    Confirm Working

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => 'DB_CONNECTION',
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,12 +41,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'DB_HOST',
-            'port' => 'DB_PORT',
-            'database' => 'DB_DATABASE',
-            'username' => 'DB_USERNAME',
-            'password' => 'DB_PASSWORD',
-            'unix_socket' => 'DB_SOCKET',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
